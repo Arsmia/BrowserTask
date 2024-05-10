471ce00680383a6077856a77769b80df5fe152df
Sviatlana Strachuk <svsa@demant.com>
2024-05-10 15:01:43 +0200

    make order with variables and empty lines

--- BrowserTask/BrowserTask/MainPage.cs
+++ BrowserTask/BrowserTask/MainPage.cs
@@ -7,9 +7,7 @@ public class MainPage(IWebDriver driver)
     private const string Url = "https://www.demant.com/";
     private IWebElement AcceptBox => driver.FindElement(By.CssSelector("[aria-label=\"I accept\"]"));
     private IWebElement AboutButton => driver.FindElement(By.CssSelector(".level1.item3.even.last"));
-
-    private IWebElement ManageAndGovern =>
-        driver.FindElement(By.CssSelector(".sub-menu a[title='Management and governance']"));
+    private IWebElement ManageAndGovern => driver.FindElement(By.CssSelector(".sub-menu a[title='Management and governance']"));
 
     public MainPage OpenFirstPage()
     {
